36ef8738113343a7ec342975797f1d16a92d154b
Innocent Anozie <iygeal@gmail.com>
2024-03-19 07:36:04 +0100

    Updated BaseModel to include kwargs

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,25 @@ from uuid import uuid4
 class BaseModel:
     """The BaseModel class from which other classes inherit"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """BaseModel constructor method"""
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+        # Check if kwargs is not empty
+        if kwargs:
+            for key, value in kwargs.items():
+                # Exclude __class__
+                if key != "__class__":
+                    # Convert isoformat strings back to datetime objects
+                    if key in ['created_at', 'updated_at']:
+                        self.__dict__[key] = datetime.fromisoformat(value)
+                    else:
+                        # Set other keys to their appropriate values
+                        setattr(self, key, value)
+        else:
+            # If kwargs is empty, create new attributes
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Visualization method for the BaseModel class"""
@@ -27,7 +41,7 @@ class BaseModel:
         """Returns a dict with all keys/values of dict of obj"""
 
         # Make a copy of the instance dictionary
-        obj_dict = self.__dict__.copy()
+        obj_dict = self.__dict__.copy() # dict(self.__dict__)
 
         # Iterate through the key-value pairs and format datetime to isoformat
         for key, value in obj_dict.items():
