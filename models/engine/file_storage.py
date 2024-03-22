bedd58b3067d6dcddc23bb340f44f0ef1262fe95
Innocent Anoziee <iygeal@gmail.com>
2024-03-22 17:41:49 +0100

    Pycodestyle fix on FileStorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,43 +5,42 @@ from models.base_model import BaseModel
 
 
 class FileStorage:
-	""" A class that stores objects in a json file"""
+    """ A class that stores objects in a json file"""
 
-	__file_path = "file.json"
-	__objects = {}
+    __file_path = "file.json"
+    __objects = {}
 
+    def all(self):
+        """Returns the dict '__objects'"""
 
-	def all(self):
-		"""Returns the dict '__objects'"""
+        return FileStorage.__objects
 
-		return FileStorage.__objects
+    def new(self, obj):
+        """Adds a new object to the '__objects' dict"""
 
-	def new(self, obj):
-		"""Adds a new object to the '__objects' dict"""
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
-		FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+    def save(self):
+        """Serializes the obj to json and saves to a file"""
 
-	def save(self):
-		"""Serializes the obj to json and saves to a file"""
+        obj_dict = {}
+        for key, value in FileStorage.__objects.items():
+            obj_dict[key] = value.to_dict()
+        # Convert the dictionary to a JSON string using json.dumps
+        json_str = json.dumps(obj_dict)
 
-		obj_dict = {}
-		for key, value in FileStorage.__objects.items():
-			obj_dict[key] = value.to_dict()
-		# Convert the dictionary to a JSON string using json.dumps
-		json_str = json.dumps(obj_dict)
+        # Open the file in write mode with UTF-8 encoding
+        with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
+            # Write the JSON string to the file
+            f.write(json_str)
 
-		# Open the file in write mode with UTF-8 encoding
-		with open(FileStorage.__file_path, 'w', encoding='utf-8') as f:
-			# Write the JSON string to the file
-			f.write(json_str)
-
-	def reload(self):
-		"""Deserializes a json file"""
-		try:
-			with open(FileStorage.__file_path, 'r', encoding='utf-8') as jf:
-				dict_obj = json.loads(jf.read())
-				for key, value in dict_obj.items():
-					obj = BaseModel(**dict_obj[key])
-					FileStorage.__objects[key] = obj
-		except FileNotFoundError:
-			pass
+    def reload(self):
+        """Deserializes a json file"""
+        try:
+            with open(FileStorage.__file_path, 'r', encoding='utf-8') as jf:
+                dict_obj = json.loads(jf.read())
+                for key, value in dict_obj.items():
+                    obj = BaseModel(**dict_obj[key])
+                    FileStorage.__objects[key] = obj
+        except FileNotFoundError:
+            pass
